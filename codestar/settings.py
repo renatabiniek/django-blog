add73a817c0ab58b1b12f21b87428bcec4c30a54
renatabiniek <renata.biniek@gmail.com>
2022-06-16 20:47:24 +0000

    Deploy 3

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['mb2test.herokuapp.com', 'localhost']
 
 # Application definition
 
